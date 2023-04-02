f91d2592673e912ac709e13e518749129cf3298e
SaturnPatrol <nikita-amirumov@mail.ru>
2023-04-02 22:47:29 +0300

    второе задание второй домашки

---
+++ dz_2/02_02.py
@@ -0,0 +1,24 @@
+import random
+
+balance = 10000
+kubik = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+bet_log = []
+while (balance > 999):
+        predict = int(input('Угадай число..? '))
+        result = random.choice(kubik)
+        if predict == result:
+            balance += 1000
+            print('Игра загадала: ' + str(result) + ', моя попытка: ' + str(predict) + ', на счету: ' + str(balance))
+            bet_log.append('Игра загадала: ' + str(result) + ', моя попытка: ' + str(predict) + ', на счету: ' + str(balance))
+        elif (predict < 2 or predict > 12):
+            print('некорректный кубик')
+            break
+        else:
+            balance -= 1000
+            print('Игра загадала: ' + str(result) + ', моя попытка: ' + str(predict) + ', на счету: ' + str(balance))
+            bet_log.append('Игра загадала: ' + str(result) + ', моя попытка: ' + str(predict) + ', на счету: ' + str(balance))
+
+#Выводим журнал ставок построчно:
+for i in bet_log:
+    print(i)
+

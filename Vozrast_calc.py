3c9b0c334dc14aa488d2fa07eddcba720490c78b
SaturnPatrol <nikita-amirumov@mail.ru>
2023-04-02 05:58:02 +0300

    первое задание

---
+++ Vozrast_calc.py
@@ -0,0 +1,5 @@
+dest_year = int(input('К какому году нужно посчитать возраст? '))
+your_year = int(input('В каком году вы родились? '))
+
+answer = dest_year - your_year
+print(str(answer) + ' года')
